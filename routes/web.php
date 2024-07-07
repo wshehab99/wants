c696a7500dc26ab933deee348fccfd6a00066651
Waleed Bin Shehab <waleedbinshihab@gmail.com>
2024-07-07 08:57:16 +0300

    fix bugs
    Co-Authored-By: Mohammed Mazhar <121592398+mohammedmazher1@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\WantController;
+use App\Http\Middleware\Any;
 use App\Models\Category;
 use App\Models\Comment;
 use App\Models\Want;
 
-Route::get('/', function () {
-    $categories =  Category::all();
-    $query =  Want::getQuery();
-    foreach ($categories as $value) {
-        $query->selectRaw("count(case when category_id = '" . $value->id . "' then 1 end ) as " . $value->name);
-    }
-    return view('welcome', [
-        'query' => $query->first(),
-        'categories' => $categories,
-    ]);
-});
-Route::prefix('admin')->group(function () {
-    Route::prefix('wants')->group(function () {
-        Route::get('/', [WantController::class, 'index']);
+Route::middleware(Any::class)->group(function () {
+    Route::get('/', function () {
+        $categories =  Category::all();
+        $query =  Want::getQuery();
+        foreach ($categories as $value) {
+            $query->selectRaw("count(case when category_id = '" . $value->id . "' then 1 end ) as " . $value->name);
+        }
+        session()->put('name', 'aaaaa');
+        return view('welcome', [
+            'query' => $query->first(),
+            'categories' => $categories,
+        ]);
     });
-});
-Route::prefix('wants')->group(function () {
-    Route::get('/{id}', [WantController::class, 'show']);
+    Route::prefix('admin')->group(function () {
+        Route::prefix('wants')->group(function () {
+            Route::get('/', [WantController::class, 'index']);
+        });
+    });
+    Route::prefix('wants')->group(function () {
+        Route::get('/{id}', [WantController::class, 'show']);
+    });
 });
